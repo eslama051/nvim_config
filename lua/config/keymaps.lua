021d2b456ca4e868d405f5c0e3a7d97b57653729
eslama051 <eslama051@gmail.com>
2023-12-09 23:01:20 +0200

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+local keymap = vim.keymap 
+local opts = {noremap = true  , silent = true }
+-- Driectory Navigation
+keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>", opts)
+keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", opts)
+
+-- Pane Navigation 
+keymap.set("n", "<C-h>" , "<C-w>h" , opts) -- navigate right 
+keymap.set("n", "<C-l>" , "<C-w>l" , opts) -- navigate left 
+keymap.set("n", "<C-j>" , "<C-w>j" , opts) -- navigate up 
+keymap.set("n", "<C-k>" , "<C-w>k" , opts) -- navigate down 
+
+-- window mangment 
+keymap.set("n" , "<leader>sv" , ":vsplit<CR>" , opts) -- Split vertically  
+keymap.set("n" , "<leader>sh" , ":split<CR>" , opts) -- Split horizontally  
+keymap.set("n" , "<leader>sm" , ":MaximizerToggle<CR>" , opts) -- toggle minimize  
