9395741d4818462e783f1ab82df13a899d3a9ffb
eslama051 <eslama051@gmail.com>
2023-12-12 08:53:12 +0200

    edits

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,3 +43,6 @@ opt.modifiable = true
 --	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
 opt.showmode = true  
+opt.arabicshape = true 
+-- opt.arabic = true
+opt.termbidi = true 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+  "folke/neodev.nvim",  
+  "nvim-tree/nvim-web-devicons"
 }
